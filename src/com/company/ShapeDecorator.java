d9f324accf575f79b431404de531a84c81a10a01
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-05-05 20:11:43 +0530

    ShapeDecorator class added

---
+++ src/com/company/ShapeDecorator.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public abstract class ShapeDecorator implements Shape {
+    protected Shape decoratedShape;
+
+    public ShapeDecorator (Shape shape){
+        this.decoratedShape = shape;
+    }
+
+    public void draw(){
+        decoratedShape.draw();
+    }
+}
