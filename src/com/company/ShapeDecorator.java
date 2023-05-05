6885917006f636f119a0fc56671da1f384a8b974
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-05-05 21:26:54 +0530

    updated

--- src/com/company/ShapeDecorator.java
+++ src/com/company/ShapeDecorator.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public abstract class ShapeDecorator implements Shape {
-    protected Shape decoratedShape;
+    protected Shape decoratedShape;//all the decorators should have a shape to decorate
 
     public ShapeDecorator (Shape shape){
         this.decoratedShape = shape;
