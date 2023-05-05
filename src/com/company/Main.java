d38e63f65775e9bf93186cb6a3f2b29ec9372460
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-05-05 19:56:28 +0530

    create objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	
+
+        Circle circle = new Circle();
+        circle.draw();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.draw();
+
+        Triangle triangle = new Triangle();
+        triangle.draw();
+
     }
 }
+
+//an application which can be used to draw different shapes
